7e9b7b4903ea8493f4b57d9dc2b2084a8c63db15
Anwer2-885 <91675160+Anwer2-885@users.noreply.github.com>
2026-09-19 01:46:18 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# ضبط موديل Xiaomi CR6609 تلقائياً
+# ضبط موديل Xiaomi CR6609
 echo 'CONFIG_TARGET_ramips=y' >> .config
 echo 'CONFIG_TARGET_ramips_mt7621=y' >> .config
 echo 'CONFIG_TARGET_ramips_mt7621_DEVICE_xiaomi_cr6609=y' >> .config
