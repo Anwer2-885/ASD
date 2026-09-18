999d9c00e073a2f265690bddedf40c18e22f13ce
Anwer2-885 <91675160+Anwer2-885@users.noreply.github.com>
2026-09-19 02:46:22 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-# ضبط موديل Xiaomi CR6609
-echo 'CONFIG_TARGET_ramips=y' >> .config
-echo 'CONFIG_TARGET_ramips_mt7621=y' >> .config
-echo 'CONFIG_TARGET_ramips_mt7621_DEVICE_xiaomi_cr6609=y' >> .config
-
-# رفع قوة البث إلى 37 dBm
-sed -i 's/20 dBm/37 dBm/g' package/firmware/wireless-regdb/files/db.txt
-sed -i 's/24 dBm/37 dBm/g' package/firmware/wireless-regdb/files/db.txt
-sed -i 's/27 dBm/37 dBm/g' package/firmware/wireless-regdb/files/db.txt
+# No custom script needed
